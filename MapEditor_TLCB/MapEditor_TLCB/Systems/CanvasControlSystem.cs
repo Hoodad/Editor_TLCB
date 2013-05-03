b5307bc64b003dfafcbc1ebff16be43dc5f90c8f
Johan Carlberg <johan.carlsberg@gmail.com>
2013-05-03 12:34:08 +0200

    Scroll zoom is now pr0nish :)

--- MapEditor_TLCB/MapEditor_TLCB/Systems/CanvasControlSystem.cs
+++ MapEditor_TLCB/MapEditor_TLCB/Systems/CanvasControlSystem.cs
@@ -24,20 +24,30 @@ namespace MapEditor_TLCB.Systems
 				MouseState currentState = Mouse.GetState();
 				Vector2 mouseDiff = new Vector2(currentState.X - previousState.X,
 					currentState.Y - previousState.Y);
+				
 				if (currentState.MiddleButton == ButtonState.Pressed &&
 					previousState.MiddleButton == ButtonState.Pressed)
 				{
 					camTransform.position += mouseDiff;
 				}
+				
+
+
+				Vector2 mousePos = new Vector2(currentState.X, currentState.Y);
+				Vector2 worldMousePos = Vector2.Transform(mousePos, Matrix.Invert(camTransform.getMatrix()));
 
 				int scrollDiff = currentState.ScrollWheelValue - previousState.ScrollWheelValue;
-				if (scrollDiff > 0)
+				if (scrollDiff > 0 && camTransform.scale <= 4.0f)
 				{
 					camTransform.scale *= 1.15f;
+					Vector2 worldMousePosChanged = Vector2.Transform(mousePos, Matrix.Invert(camTransform.getMatrix()));
+					camTransform.position += (worldMousePosChanged - worldMousePos) * camTransform.scale;
 				}
-				else if (scrollDiff < 0)
+				else if (scrollDiff < 0 && camTransform.scale >= 0.2f)
 				{
 					camTransform.scale /= 1.15f;
+					Vector2 worldMousePosChanged = Vector2.Transform(mousePos, Matrix.Invert(camTransform.getMatrix()));
+					camTransform.position += (worldMousePosChanged - worldMousePos) * camTransform.scale;
 				}
 
 				previousState = currentState;
