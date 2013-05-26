dacfc68519e3397b85c559556c924001a432256e
Robin Thunström <Robin.thunstrom@gmail.com>
2013-05-26 13:26:01 +0200

    Fixed: *Shortcut assignment to Close in radial menu causes the game to crash.

--- MapEditor_TLCB/MapEditor_TLCB/Editor.cs
+++ MapEditor_TLCB/MapEditor_TLCB/Editor.cs
@@ -214,9 +214,6 @@ namespace MapEditor_TLCB
 			{
 				textures.Add("tilemap_winecellar", Texture2D.FromStream(graphics.GraphicsDevice, fileStream));
 			}
-
-			//textures.Add("tilemap_garden", Content.Load<Texture2D>("TileSheets/tilemap_garden"));
-			//textures.Add("tilemap_winecellar", Content.Load<Texture2D>("TileSheets/tilemap_winecellar"));
 			textures.Add("debugBlock", Content.Load<Texture2D>("debugBlock"));
 			textures.Add("canvas_shadow", Content.Load<Texture2D>("canvas_shadow"));
 			textures.Add("canvas_shadow_10px", Content.Load<Texture2D>("canvas_shadow_10px"));
